1779ed0026005adc57e6029398a6e1b8af7fb6af
phuan <289945752@qq.com>
2023-09-22 12:26:11 +0800

    create repo

---
+++ expressMS_html/js/login.js
@@ -0,0 +1,74 @@
+var flag = 0; // 1表示明文显示， 0表示密文显示
+
+function login1() {
+	// alert("登录成功");
+	var acc1 = document.getElementById("account1").value;
+	var pw1 = document.getElementById("pw1").value;
+	var inputCaptcha = document.getElementById("input_captcha").value;
+	// 检查用户名和密码是否为空
+	if (acc1 === "") {
+		alert("用户名不能为空！");
+		return;
+	}
+	if (pw1 === "") {
+		alert("密码不能为空！");
+		return;
+	}
+	
+	// 检查输入的验证码是否正确
+	if(inputCaptcha != window.generatedCaptcha) {
+	    alert("验证码错误");
+		return;
+	}
+	
+	if (acc1 === "admin" && pw1 === "111") {
+		alert("登录成功");
+	} else {
+		alert("登录失败，用户名或密码错误");
+	}
+}
+
+// 图片点击+密码框修改
+function changeEye() {
+	var pw1 = document.getElementById("pw1");
+	var eye = document.getElementById("eye");
+
+	if (flag == 1) {
+		pw1.type = "password";
+		eye.src = "img/eye_open.png";
+		flag = 0;
+	} else {
+		pw1.type = "text";
+		eye.src = "img/eye_close.png";
+		flag = 1;
+	}
+}
+// 	// BOM
+// $("button").click(function(){
+// 	$(this).css("background", "pink")
+// 	$(this).siblings("button").css("background","")
+// }
+// )
+
+// 生成一个四位数的随机验证码
+function generateCaptcha() {
+    var ctx = document.getElementById("captchaCanvas").getContext("2d");
+    ctx.clearRect(0, 0, 100, 40); // 清除之前的内容
+
+    var captcha = "";
+    for(var i = 0; i < 4; i++) {
+        var num = Math.floor(Math.random() * 10);
+        captcha += num;
+    }
+
+    // 设置字体、颜色等
+    ctx.font = "30px Arial";
+    ctx.fillStyle = "black";
+    ctx.fillText(captcha, 10, 20);
+
+    // 将验证码存储为全局变量或其他方法，以便验证
+    window.generatedCaptcha = captcha;
+}
+
+// 在页面加载时生成验证码
+window.onload = generateCaptcha;
---
+++ expressMS_html/js/signup.js
@@ -0,0 +1,83 @@
+var flag = 0; // 1表示明文显示， 0表示密文显示
+// 图片点击+密码框修改
+function changeEye(id) {
+	var pw = document.getElementById("password");
+	var eye = document.getElementById("eye");
+
+	if (flag == 1) {
+		pw.type = "password";
+		eye.src = "img/eye_open.png";
+		flag = 0;
+	} else {
+		pw.type = "text";
+		eye.src = "img/eye_close.png";
+		flag = 1;
+	}
+}
+
+function signup() {
+	var phone = document.getElementById("phone").value;
+	var nickname = document.getElementById("nickname").value;
+	var password = document.getElementById("password").value;
+	var confirmPassword = document.getElementById("confirmPassword").value;
+	var verificationCode = document.getElementById("verificationCode").value;
+	var genderRadios = document.getElementsByName("SEX");
+	var gender = "";
+
+	for (var i = 0; i < genderRadios.length; i++) {
+		if (genderRadios[i].checked) {
+			gender = genderRadios[i].value;
+			break;
+		}
+	}
+
+	if (!phone) {
+		alert("手机号不能为空！");
+		return;
+	}
+
+	// 验证手机号是否为11位数字
+	if (!/^\d{11}$/.test(phone)) {
+		alert("无效的手机号");
+		return;
+	}
+
+	if (!nickname) {
+		alert("昵称不能为空！");
+		return;
+	}
+	
+	// 避免基础的非法字符输入
+	var illegalCharacters = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
+	if (illegalCharacters.test(phone) || illegalCharacters.test(nickname)) {
+		alert("请不要输入非法字符");
+		return;
+	}
+
+	if (!password) {
+		alert("密码不能为空！");
+		return;
+	}
+
+	if (password.length < 6) {
+		alert("密码长度必须至少为6位！");
+		return;
+	}
+
+	if (password !== confirmPassword) {
+		alert("两次输入的密码不匹配！");
+		return;
+	}
+
+	if (!verificationCode) {
+		alert("验证码不能为空！");
+		return;
+	}
+
+	if (!gender) {
+		alert("请选择性别！");
+		return;
+	}
+	// 与服务器交互的代码，比如使用AJAX来发送数据
+	alert("注册成功！");
+}
