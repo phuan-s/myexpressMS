5e22a8465fa9914e47a839d92c04d3f3618e9f89
phuan <289945752@qq.com>
2023-10-09 10:18:55 +0800

    1009

---
+++ java/src/servlet/LoginServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", urlPatterns = {"/doLogin"})
+public class LoginServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        System.out.println("一段中文");
+        String acc = request.getParameter("acc");
+        String pwd = request.getParameter("pwd");
+        System.out.println("acc:" + acc + ",pwd:" + pwd);
+        return;
+    }
+}
